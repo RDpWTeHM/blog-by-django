fe608e3809bddfcc65860aa69695932b3606d8f8
Joseph Lin <joseph.lin@aliyun.com>
2019-04-08 00:18:05 +0800

    [develop] basic login function - use new traceless window of browser to test    1. request "/posts/" then request "/posts/create/" it will be 404    2. request "/login/" login first, then "/posts/create/" it will work

--- blog/src/accounts/views.py
+++ blog/src/accounts/views.py
@@ -15,7 +15,9 @@ def login_view(request):
     if form.is_valid():
         username = form.cleaned_data.get("username")
         password = form.cleaned_data.get("password")
-
+        user = authenticate(username=username, password=password)
+        login(request, user)
+        # redirect
     return render(request, "login.html",
                   {"form": form, "title": "Login", })
 
