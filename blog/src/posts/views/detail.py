923fff24a9613895c72cdb76e70f556687eb56e1
Joseph Lin <joseph.lin@aliyun.com>
2019-04-07 18:26:14 +0800

    [Refactoring] detail function in `views/detail.py`

--- blog/src/posts/views/detail.py
+++ blog/src/posts/views/detail.py
@@ -1,22 +1,26 @@
 # posts/views/detail.py
+"""
+functional:
+  - display post detail
+  - logged-in user could comment the post(create comment)
+
+"""
 
 from . import *
 
 
-def detail(request, slug):
-    post = get_object_or_404(Post, slug=slug)
-    if post.draft or post.publish > timezone.now().date():
-        if not request.user.is_staff or not request.user.is_superuser:
-            raise Http404
-
+def support_comment(request, post: 'Post') -> CommentForm:
     # best thing here - initial Foreign thing at the get before the post
     initial4comment = {
         'content_type': post.get_content_type,
         'object_id': post.id,
     }
+
     comment_form = CommentForm(request.POST or None, initial=initial4comment)
+
     if comment_form.is_valid():
         dbg_print(comment_form.cleaned_data)
+
         c_type = comment_form.cleaned_data.get("content_type")
         content_type = ContentType.objects.get(model=c_type)
         obj_id = comment_form.cleaned_data.get("object_id")
@@ -31,10 +35,21 @@ def detail(request, slug):
         if created:
             dbg_print("It works!")
 
+    return comment_form
+
+
+def detail(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    if post.draft or post.publish > timezone.now().date():
+        if not request.user.is_staff or not request.user.is_superuser:
+            raise Http404
+
+    form = support_comment(request, post=post)
+
     context = {
         "title": "Detail",
         "post": post,
         # 'comments': Comment.objects.filter_by_instance(post),
-        'comment_form': comment_form,
+        'comment_form': form,
     }
     return render(request, "posts/detail.html", context=context)
